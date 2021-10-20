a849ea988c1dc2d2a311aa9c8ee5034cf4ecd862
Jesse McDonald <jessehmcdonald@gmail.com>
2021-10-20 18:48:27 -0500

    Updated motor object

--- SUB3/motor_management.py
+++ SUB3/motor_management.py
@@ -1,24 +1,50 @@
 import serial
+from time import sleep
+import threading
 BAUD = 115200
 
 class motor:
-    def __init__(self,com):
+    def __init__(self, com, max, min):
         self.com = com
         self.ser = serial.Serial(com, BAUD, timeout=.1)
-    
-    def communicator(self):
+        self.preload_max = max
+        self.preload_min = min
+        self.torque_update = False
+        self.torque = 0
+        self.torque_thread = None
+
+    def enable(self):
         self.ser.write("a".encode())
-        while(True):
-            
-            data_from_esp = self.ser.readline().decode().strip()
-            print("READ",data_from_esp)
-            if data_from_esp[0:4]=="TOR":
-                torque = float(data_from_esp.split(":")[1])
-                print(torque)
+        #TODO Add ack checks
+    
 
+    def torque(self):
+        print("A")
+        # print(self.read_torque)
+        # while(self.read_torque):
+        #     data_from_ser = self.ser.readline().decode()
+        #     print(data_from_ser)
 
-
-
+    # This function stops all processes and disables motor
+    def stop(self):
+        self.ser.write("d".encode())
+        #TODO Add ack checks
+        
+        #Stop the torque input thread
+        if self.torque_thread:
+            self.read_torque = False
+            self.torque_thread.join()
+    
+    # This function starts the torque capturing process
+    def start_torque_readings(self):
+        self.read_torque = True
+    
+        torque_thread = threading.Thread(target=self.torque)
+        sleep(1)
+        # print(self.torque_thread.is_alive())
+        torque_thread.start()
+        print(torque_thread.is_alive())
+   
 
 
 
@@ -28,8 +54,12 @@ class motor:
 
 
 def main():
-    mot = motor("COM15")
-    mot.communicator()
+    mot = motor("COM15", .51,.53)
+    mot.enable()
+    mot.start_torque_readings() 
+    sleep(10)
+    mot.stop()
+
 
 
 
