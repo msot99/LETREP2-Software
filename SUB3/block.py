2b4b243b68e358b98469e46e201713020440053c
Jesse McDonald <jessehmcdonald@gmail.com>
2021-10-27 16:42:35 -0500

    Added objects for storing trials and blocks

---
+++ SUB3/block.py
@@ -0,0 +1,18 @@
+from datetime import date
+
+
+class block():
+    def __init__(self, patID = 1234, dte = str(date.today()), sess = 1, blocknum = 1 ):
+        self.patID = patID 
+        self.date = dte
+        self.session = sess
+        self.blocknum = blocknum
+        self.trials = list()
+
+    def compute_avg_success(self) -> None:
+        #TODO compute average success rate based off of data in trails
+        pass
+    
+    def number_of_trials(self):
+        return len(self.trials)
+        
---
+++ SUB3/trial.py
@@ -0,0 +1,14 @@
+
+
+class trial():
+    def __init__(self):
+        self.emg_data = []
+        self.success = False
+        self.peak = 0
+        self.failure-reason = "none" 
+        """
+            Potential failure reasons
+            "prelow" - Patient failed by not maintaining preload on low end
+            "prehigh" - Patient failed by not maintaining preload on high end
+            "mwave" - Patient failed by having too large a peak mwave
+        """
