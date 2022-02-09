6441f51762bb4c9af8b56d8892c0581480a59db9
Budmeister <74798905+Budmeister@users.noreply.github.com>
2022-02-08 23:02:13 -0600

    Finished populating More Options menu

--- SUB3/more_options.py
+++ SUB3/more_options.py
@@ -1,9 +1,32 @@
 from tkinter import *
 from global_funcs import *
 
-def show_more_options(port, pat_id, sess, no_motor, no_emg):
+class Options:
+    def __init__(self, port, pat_id, sess):
+        self.port = port
+        self.pat_id = pat_id
+        self.sess = sess
+        self.sess_updated = False
+    
+    def copy(self):
+        return Options(self.port, self.pat_id, self.sess)
+
+    def impose_on(self, other):
+        other.port = self.port
+        other.pat_id = self.pat_id
+        if other.sess != self.sess:
+            other.sess = self.sess
+            other.sess_updated = True
+            print("sess_updated")
+        else:
+            other.sess = self.sess
+
+def show_more_options(options):
     root = Tk()
+    root.running = True
     root.configure(bg="white")
+
+    modified_options = options.copy()
 
     title_label = Label(root, text="More Options", bg="white", padx=padx, pady=pady, font=large_font)
     title_label.grid(row=0, column=0, columnspan=2)
@@ -12,7 +35,7 @@ def show_more_options(port, pat_id, sess, no_motor, no_emg):
     port_label.grid(row=1, column=0, sticky="e")
 
     port_text = Text(root, width=8, height=1, fg='gray')
-    port_text.insert(1.0, port)
+    port_text.insert(1.0, str(options.port))
     port_text.configure(state='disabled')
     port_text.grid(row=1, column=1, sticky="w")
 
@@ -20,7 +43,7 @@ def show_more_options(port, pat_id, sess, no_motor, no_emg):
     id_label.grid(row=2, column=0, sticky="e")
     
     id_text = Text(root, width=8, height=1, fg='gray')
-    id_text.insert(1.0, str(pat_id))
+    id_text.insert(1.0, str(options.pat_id))
     id_text.configure(state='disabled')
     id_text.grid(row=2, column=1, sticky="w")
 
@@ -28,21 +51,26 @@ def show_more_options(port, pat_id, sess, no_motor, no_emg):
     sess_label.grid(row=3, column=0, sticky="e")
 
     sess_choice = IntVar(root)
-    sess_choice.set(1)
+    sess_choice.set(options.sess)
 
-    sess_list = range(2, 16)
+    sess_list = range(1, 16)
 
+    def sess_command(event):
+        modified_options.sess = sess_choice.get()
     sess_selector = OptionMenu(
-        root, sess_choice, sess_choice.get(), *sess_list)
+        root, sess_choice, *sess_list, command=sess_command)
     sess_selector.configure(width=5, anchor="w")
     sess_selector.grid(row=3, column=1, sticky="w")
 
     def exit():
-        pass
+        root.running = False
+        root.destroy()
 
     def ok():
-        
+        modified_options.impose_on(options)
         exit()
+        
+    root.protocol("WM_DELETE_WINDOW", exit)
 
     ok_button = Button(root, text="Ok", command=ok, width=10, height=2)
     ok_button.grid(row=6, column=0, sticky="e", padx=padx, pady=pady)
@@ -51,10 +79,12 @@ def show_more_options(port, pat_id, sess, no_motor, no_emg):
     cancel_button.grid(row=6, column=1, sticky="w", padx=padx, pady=pady)
 
     center_window(root)
-    root.mainloop()
+    # root.mainloop()
+    while root.running:
+        root.update()
 
     
 
 
 if __name__ == "__main__":
-    show_more_options('COM1', 1234, 1, True, True)
+    show_more_options(Options('COM1', 1234, 4))
