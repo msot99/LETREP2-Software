128c44f7432e517271fcef7e42a4135080aee605
Jesse McDonald <jessehmcdonald@gmail.com>
2022-02-15 18:10:49 -0600

    GUI And Function Updates

--- SUB3/more_options.py
+++ SUB3/more_options.py
@@ -1,22 +1,30 @@
+from msilib.schema import CheckBox
 from tkinter import *
 from global_funcs import *
 
 class Options:
-    def __init__(self, port, pat_id, sess):
+    def __init__(self, port, pat_id, sess, pre_min = .4, pre_max = .5, display_T=False):
         self.port = port
         self.pat_id = pat_id
         self.sess = sess
-        self.sess_updated = False
+        self.torque_display = display_T
+        self.pre_min = pre_min
+        self.pre_max = pre_max
+        self.updates = False
+        
     
     def copy(self):
-        return Options(self.port, self.pat_id, self.sess)
+        return Options(self.port, self.pat_id, self.sess, self.torque_display)
 
     def impose_on(self, other):
+        other.updates = True
         other.port = self.port
         other.pat_id = self.pat_id
+        other.torque_display = self.torque_display
+        other.pre_max = self.pre_max
+        other.pre_min = self.pre_min
         if other.sess != self.sess:
             other.sess = self.sess
-            other.sess_updated = True
             print("sess_updated")
         else:
             other.sess = self.sess
@@ -47,8 +55,22 @@ def show_more_options(options):
     id_text.configure(state='disabled')
     id_text.grid(row=2, column=1, sticky="w")
 
+    max_label = Label(root, text="Preload Max:", bg="white", font=small_font)
+    max_label.grid(row=3,column=0,sticky="e")
+
+    max_entry = Entry(root, width=8, fg='black')
+    max_entry.insert(0, str(options.pre_max))
+    max_entry.grid(row=3, column=1, sticky="w")
+
+    min_label = Label(root, text="Preload Min:", bg="white", font=small_font)
+    min_label.grid(row=4, column=0, sticky="e")
+
+    min_entry = Entry(root, width=8, fg='black')
+    min_entry.insert(0, str(options.pre_min))
+    min_entry.grid(row=4, column=1, sticky="w")
+
     sess_label = Label(root, text="Session #:", bg="white", font=small_font)
-    sess_label.grid(row=3, column=0, sticky="e")
+    sess_label.grid(row=5, column=0, sticky="e")
 
     sess_choice = IntVar(root)
     sess_choice.set(options.sess)
@@ -60,23 +82,35 @@ def show_more_options(options):
     sess_selector = OptionMenu(
         root, sess_choice, *sess_list, command=sess_command)
     sess_selector.configure(width=5, anchor="w")
-    sess_selector.grid(row=3, column=1, sticky="w")
+    sess_selector.grid(row=5, column=1, sticky="w")
+
+    tor_disp = BooleanVar(root)
+    tor_disp.set(options.torque_display)
+
+    def tor_disp_command():
+        modified_options.torque_display = tor_disp.get()
+    torque_disp_checkbox = Checkbutton(root, bg='white', text='Display Torque',
+                                       variable=tor_disp, onvalue=True, offvalue=False, command=tor_disp_command)
+    torque_disp_checkbox.grid(row=6,column=0,columnspan=2)
+
 
     def exit():
         root.running = False
         root.destroy()
 
     def ok():
+        modified_options.pre_min = float(min_entry.get())
+        modified_options.pre_max = float(max_entry.get())
         modified_options.impose_on(options)
         exit()
         
     root.protocol("WM_DELETE_WINDOW", exit)
 
     ok_button = Button(root, text="Ok", command=ok, width=10, height=2)
-    ok_button.grid(row=6, column=0, sticky="e", padx=padx, pady=pady)
+    ok_button.grid(row=7, column=0, sticky="e", padx=padx, pady=pady)
 
     cancel_button = Button(root, text="Cancel", command=exit, width=10, height=2)
-    cancel_button.grid(row=6, column=1, sticky="w", padx=padx, pady=pady)
+    cancel_button.grid(row=7, column=1, sticky="w", padx=padx, pady=pady)
 
     center_window(root)
     # root.mainloop()
