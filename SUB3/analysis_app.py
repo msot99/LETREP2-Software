c5816d76edf0586b91dea3dd1c9ae30fc0677ad4
Jesse McDonald <jessehmcdonald@gmail.com>
2022-02-23 21:45:58 -0600

    Added ability to drive down from sess to trial
    Added a listbox to allow driving down from a session into a trial.

--- SUB3/analysis_app.py
+++ SUB3/analysis_app.py
@@ -1,4 +1,5 @@
 
+from tkinter.scrolledtext import ScrolledText
 from tkinter import *
 from tkinter import filedialog, messagebox
 from global_funcs import *
@@ -7,53 +8,157 @@ from PIL import ImageTk, Image
 from analysis_tools import *
 
 
+class analysis_app(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("LETREP2 Analysis")
+        self.configure(bg="white")
+        self.running = True
 
-def show_analysis_app():
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-    root = Tk()
-    root.title("LETREP2 Analysis")
-    root.configure(bg="white")
-    root.running = True
+        self.cbs = []
+        self.sessions = {}
 
-    def on_closing():
-        root.running = False
-        root.destroy()
+        self.browsing = False
+        self.browsing_dict = {}
+
+        # Button configuration
+        self.big_w = 32
+        self.big_h = 3
+
+        self.small_w = 10
+        self.small_h = 1
+
+        # browse_button
+        self.open_btn = Button(self, text="Open Pat folder", command=self.on_open, width=self.big_w, height=self.big_h,
+                        bg="blue", font=button_font, fg=button_font_color)
+        self.open_btn.grid(row=1, column=0, padx=padx, pady=pady)
+
+        # to csv Button
+        self.csv_btn = Button(self, text="Convert to CSV", command=self.on_open, width=self.big_w, height=self.big_h,
+                        bg="blue", font=button_font, fg=button_font_color)
+        self.csv_btn.grid(row=2, column=0, padx=padx, pady=pady)
+
+        # Browser frame
+        self.browser_frame = Frame(self, )
+        self.browser_frame.grid(row=1, column=1, columnspan = 4, rowspan = 4, padx=padx, pady=pady)
+
+        img = Image.open(logo_dir)
+        img = img.resize((100, 100), Image.ANTIALIAS)
+        logo = ImageTk.PhotoImage(img)
+        self.logo_label = Label(self, image=logo, bg="white")
+        self.logo_label.photo = logo
+        self.logo_label.grid(row=0, column=0, padx=padx, pady=pady)
         
-    root.protocol("WM_DELETE_WINDOW", on_closing)
+        # Window Configuration
+        # self.geometry("1200x600")
+        center_window(self)
+        self.title("BETA BETA BETA")
 
-    def on_open():
+
+    def on_closing(self):
+        self.running = False
+        self.destroy()
+
+    def on_browse_open(self):
+        selected = self.browse_listbox.curselection()
+        if selected:
+            selected = selected[0]
+            if "Session" in self.browse_listbox.get(selected):
+                key = int(self.browse_listbox.get(selected).split()[1])
+                self.on_browse_close(False)
+                self.start_browse(self.browsing_dict[key], "Block")
+
+            elif "Block" in self.browse_listbox.get(selected):
+
+                key = int(self.browse_listbox.get(selected).split()[1])
+                self.on_browse_close(False)
+                # Convert trial list to dictionary
+                to_browse = {i:trl for i,trl in enumerate(self.browsing_dict[key].trials)} 
+
+                self.start_browse(to_browse, "Trial")
+
+    
+    def on_browse_goback(self):
+        pass
+
+    def on_browse_close(self, delete_sessions = True):
+        self.browse_close_btn.destroy()
+        self.browse_open_btn.destroy()
+        self.browse_goback_btn.destroy()
+        self.browse_listbox.destroy()
+
+        if delete_sessions:
+            self.sessions = {}
+
+    def on_open(self):
         folder_name = filedialog.askdirectory(title="Select Patient Folder")
         if folder_name != "":
-            sessions, multiple_patids = open_json_files(folder_name)
-        
+            
+            self.sessions, multiple_patids = open_json_files(folder_name)
+            
             if multiple_patids:
                 messagebox.showwarning(
                     "Data Loading Error!", "Loaded Blocks Contain Differeing Patient IDs")
+            
+            self.start_browse(self.sessions, "Sessions")
+            
+
+    def convert_to_JSON(self):
+        folder_name = filedialog.askdirectory(title="Select Where to Save CSV")
+        for sess in self.sessions.values():
+                sess_to_csv(sess, folder_name)
+                messagebox.showinfo(
+                    "Sessions to CSV!", f"Converted {len(self.sessions)} session(s) to csv")
+
+
+    def start_browse(self, items_to_browse, items_type, multi_select = False):
+
+        self.browsing_dict = items_to_browse
+
+        # Create buttons for browsing
+        self.browse_open_btn = Button(self.browser_frame,text="Open", command=self.on_browse_open, width=self.small_w, height=self.small_h,
+                                      bg="blue", font=button_font, fg=button_font_color)
+        self.browse_open_btn.grid(
+            row=2, column=1, padx=padx, pady=pady, sticky=NE)
+
+        self.browse_goback_btn = Button(self.browser_frame,text=chr(10229), command=self.on_browse_goback, width=self.small_w, height=self.small_h,
+                                        bg="blue", font=button_font, fg=button_font_color)
+        self.browse_goback_btn.grid(
+            row=2, column=1, padx=padx, pady=pady, sticky=NW)
+
+        self.browse_close_btn = Button(self.browser_frame,text="Close browser", command=self.on_browse_close, width=self.small_w * 2, height=self.small_h,
+                                        bg="blue", font=button_font, fg=button_font_color)
+        self.browse_close_btn.grid(
+            row=3, column=1, padx=padx, pady=pady)
+
+        # Create Listbox for data
+        self.browse_listbox = Listbox(
+            self.browser_frame, width=40, height=12, selectmode=multi_select)
+        self.browse_listbox.grid(
+            row=0, column=1, rowspan=2, padx=padx, pady=pady, sticky=S)
+
+        for key, _ in items_to_browse.items():
+            self.browse_listbox.insert(key, f'{items_type} {key}')
 
         
 
-    # Button configuration
-    big_w = 18
-    big_h = 3
+    
+    def run(self):
 
-    # browse_button
-    open_btn = Button(root, text="Open Patient Folder", command=on_open, width=big_w, height=big_h,
-                       bg="blue", font=button_font, fg=button_font_color)
-    open_btn.grid(row=1, column=0, padx=padx, pady=pady)
+        while self.running:
+            if self.browsing:
+                if self.browse_listbox.curselection():
+                    self.browse_open_btn["state"] = NORMAL
+                else:
+                    self.browse_open_btn["state"] = DISABLED
 
-    img = Image.open(logo_dir)
-    img = img.resize((100, 100), Image.ANTIALIAS)
-    logo = ImageTk.PhotoImage(img)
-    logo_label = Label(root, image=logo, bg="white")
-    logo_label.grid(row=0, column=0, padx=padx, pady=pady)
-    root.geometry("1200x600")
-    center_window(root)
+            self.update()
 
-
-    while root.running:
-        root.update()
 
 
 if __name__ == "__main__":
 
-    show_analysis_app()
+    app = analysis_app()
+    app.run()
--- SUB3/analysis_tools.py
+++ SUB3/analysis_tools.py
@@ -14,7 +14,7 @@ def open_json_files(directory):
     # Get only json files
     json_files = [join(directory,f) for f in listdir(directory) if isfile(
         join(directory, f)) and splitext(join(directory, f))[1] == ".json"]
-    
+
     # Open all JSON files
     for f in json_files:
         
@@ -22,18 +22,37 @@ def open_json_files(directory):
             jdict = json.load(json_file)
             blk = block(jdict["block"])
             if blk.session in sessions.keys():
-                sessions[blk.session].append(blk)
+                sessions[blk.session][blk.blocknum] = blk
             else:
-                sessions[blk.session] = [blk]
+                sessions[blk.session] = {blk.blocknum: blk}
+                
 
     patids = []
     for sess in sessions.values():
-        for blk in sess:
+        for blk in sess.values():
             if blk.patID not in patids:
                 patids.append(blk.patID)
     if len(patids)>1:
         return sessions, True
 
     return sessions, False
+
+def sess_to_csv(sess, folder_name):
+    for blk in sess:
+        with open(join(folder_name,f"Block{blk.blocknum}_{blk.date}.csv"), "w") as csv_file:
+            trls = list(blk.trials)
+            for i, trl in enumerate(trls):
+                csv_file.write(f"Trial {i} emg, Trial {i} acc,")
+            csv_file.write("\n")
+            max_len = max([len(data) for trl in trls for data in (trl.emg_data, trl.acc_data)])
+            for i in range(max_len):
+                for trl in trls:
+                    if len(trl.emg_data) > i:
+                        csv_file.write(str(trl.emg_data[i]))
+                    csv_file.write(",")
+                    if len(trl.acc_data) > i:
+                        csv_file.write(str(trl.acc_data[i]))
+                    csv_file.write(",")
+                csv_file.write("\n")
 
 
