b8b04d206b232cb4cbfca2be2ed83ecd22ff03dc
ChrismanC <80658083+ChrismanC@users.noreply.github.com>
2022-02-26 12:27:11 -0600

    Update peak.py

--- SUB3/peak.py
+++ SUB3/peak.py
@@ -1,59 +1,77 @@
 from scipy import signal
+freq=1925
 
 def simple_peak(trialdata):
-  freq=1925
-  wavelength=.005
+    freq=1925
+    wavelength=.005
 
-  emg=trialdata.emg_data
-  acc=trialdata.acc_data
+    emg=trialdata.emg_data
+    acc=trialdata.acc_data
 
-  start_acc=acc[0]
+    start_acc=acc[0]
 
-  for i in range(len(acc)):
-      if acc[i]!=start_acc:
-          fire=i
-          break
+    for i in range(len(acc)):
+        if acc[i]!=start_acc:
+            fire=i
+            break
 
-  maximum=[650]
-  maximum[0]=0
-  n=0
-  increase=False
+    maximum=[650]
+    maximum[0]=0
+    n=0
+    increase=False
 
-  for j in range(int((fire+(.04*1925))),int((fire+(.085*1925)))):
-      if emg[j]>=maximum[n]:
-          if increase:
-              maximum.append(emg[j])
-              increase=False
-              n+=1
-          else:
-              maximum[n]=emg[j]
-          max_samp=j
-      elif j>(max_samp+((wavelength/2)*freq)):
-          increase=True
+    for j in range(int((fire+(.04*1925))),int((fire+(.085*1925)))):
+        if emg[j]>=maximum[n]:
+            if increase:
+                maximum.append(emg[j])
+                increase=False
+                n+=1
+            else:
+                maximum[n]=emg[j]
+            max_samp=j
+        elif j>(max_samp+((wavelength/2)*freq)):
+            increase=True
 
-  peak=maximum[-1]
+    peak=maximum[-1]
 
-  return peak
+    return peak
 
-def wave_avg(trialdata):
+
+def base_peak(trialdata):
+
     emg=trialdata.emg_data
 
-    maxes=signal.find_peaks(emg, height=.06, distance=10)[0]
+    maxes=signal.find_peaks(emg, height=.15)[0]
 
-    print(maxes)
+    peak=maxes[0]
 
-    peak_dis=1000
-    for i in range(len(maxes)):
-        if(abs(maxes[i]-596)<peak_dis):
-            peak_dis=abs(maxes[i]-596)
-            peak=maxes[i]
+    sum=0
+    for i in range((peak-10),(peak+10)):
+        sum=sum+emg[i]
+    
+    avg_peak=sum/20
+    peak_ms = (peak-500)/freq * 1000
+
+    return(avg_peak, peak_ms)
+
+def condition_peak(trialdata):
+    avg_time=36.36
+
+    M1_avg=(avg_time*freq/1000)+500
+
+    emg=trialdata.emg_data[M1_avg-20:M1_avg+20]
+
+    maxes=signal.find_peaks(emg, height=.1)[0]
+
+    peak=maxes[0]
 
     sum=0
     for i in range((peak-10),(peak+10)):
         sum=sum+emg[i]
 
     avg_peak=sum/20
+    peak_ms = (peak-500+M1_avg)/freq * 1000
 
-    return(avg_peak)
+    return(avg_peak, peak_ms)
 
 
